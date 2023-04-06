2da01a6e9f057d526de9268aae16dab6392c6c4d
Armando Castillo Hernandez <armando@me.com>
2023-04-06 12:14:21 -0600

    Add sentry.io

--- App.tsx
+++ App.tsx
@@ -7,6 +7,14 @@ import { AuthNavigator } from './src/navigator/AuthNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { HorasProvider } from './src/context/HorasContext';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://ba39ed51b69349c8a9cc830f199dc263@o4503943605387264.ingest.sentry.io/4504930005811200',
+    environment: 'develop',
+});
+
+
 const AppState = ({ children }: any) => {
   return (
     <AuthProvider>
