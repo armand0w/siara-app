3a3965abc0ee8e6d1e0bbb241a9e8c59f231ba80
Armando Castillo Hernandez <armando@me.com>
2023-04-18 08:30:41 -0600

    Se elimina uso de LoadingScreen

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
   dsn: 'https://ba39ed51b69349c8a9cc830f199dc263@o4503943605387264.ingest.sentry.io/4504930005811200',
-    environment: 'develop',
+  environment: 'develop',
 });
 
 
